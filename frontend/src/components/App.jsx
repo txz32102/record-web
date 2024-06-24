9faba78a376ebf14a44366225ce20127c6dfb0bd
musong <txz32102@gmail.com>
2024-06-25 07:13:01 +0800

    add readme

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import View from './BaseView/View';
-import Moment from './Moment/Moment'
+import Moment from './Moment/Moment';
+import Readme from './ReadmeRenderer/ReadmeRenderer';
+import ReadmeViewer from './ReadmeRenderer/ReadmeViewer';
 
 const App = () => {
-
   return (
-    <div className="app">
-      <View/>
-      <Moment/>
-    </div>
+    <Router>
+      <div className="app">
+        <View />
+        <Routes>
+           <Route path="/" element={<Moment />} />
+          <Route path="/readme" element={<Readme />} />
+          <Route path="/readme/:filename" element={<ReadmeViewer />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
