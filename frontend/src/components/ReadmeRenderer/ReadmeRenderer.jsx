461f7418dbea2b5dbc2598723a1931bd5a068c79
musong <txz32102@gmail.com>
2024-06-25 07:13:22 +0800

    9001 now

--- frontend/src/components/ReadmeRenderer/ReadmeRenderer.jsx
+++ frontend/src/components/ReadmeRenderer/ReadmeRenderer.jsx
@@ -1,18 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const ReadmeRenderer = () => {
-  const [content, setContent] = useState('');
+  const [files, setFiles] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    // Fetch the README content from your Python server
-    fetch('/api/readme')
-      .then(response => response.text())
-      .then(data => setContent(data));
+    const fetchData = async () => {
+      try {
+        const apiUrl = process.env.REACT_APP_API_URL || "http://localhost:9001";
+        const url = `http://localhost:9001/files`
+        const response = await axios.get(url);
+        console.log(url);
+        if (Array.isArray(response.data.files)) {
+          setFiles(response.data.files);
+          console.log(response.data.files)
+        } else {
+          throw new Error("API response is not an array");
+        }
+      } catch (error) {
+        console.error("Error fetching files:", error);
+        setError(null);
+      }
+    };
+
+    fetchData();
   }, []);
 
   return (
-    <div className="readme-renderer">
-      <pre>{content}</pre>
+    <div>
+      <p>hello world</p>
+      {error ? (
+        <div>Error: {error.message}</div>
+      ) : (
+        <ul>
+          {files.map((file, index) => (
+            <li key={index}>
+              <Link to={`/readme/${file}`}>{file}</Link>            
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
